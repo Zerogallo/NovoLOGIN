69d1b90aa923a3db5d04852bb91409e33749309e
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-05-27 20:55:24 -0300

    up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./login/indexlogin";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/login/indexlogin.jsx
@@ -0,0 +1,38 @@
+import { GiAstronautHelmet } from "react-icons/gi";
+import styles from "./stylesLogin.module.css";
+import { PiPlanetDuotone } from "react-icons/pi";
+import imgLogin from "../../public/Free-Astronaut.jpg";
+
+export default function Login() {
+  return (
+    <>
+      <div className={styles.login}>
+        <div className={styles.caixaText}>
+          <i>
+            <GiAstronautHelmet />
+          </i>
+          <span>Conheça novo Planeta</span>
+          <i>
+            <PiPlanetDuotone />
+          </i>
+          <p>E site para encontrar novo mundo pela internet</p>
+
+          <img src={imgLogin} alt="astronauta" />
+        </div>
+        <div className={styles.caixaLogin}>
+          <i>
+            <GiAstronautHelmet />
+          </i>
+          <p>Login</p>
+          <div>
+            <p>E-mail</p>
+
+            <input type="text" />
+            <p>Senha</p>
+            <input type="password" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
