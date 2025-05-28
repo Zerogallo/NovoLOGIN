224bf7d87f730fd4ccf5cb4dbe48df9a32e813c5
Zerogallo <167368572+Zerogallo@users.noreply.github.com>
2025-05-28 10:31:00 -0300

    up

--- src/login/indexlogin.jsx
+++ src/login/indexlogin.jsx
@@ -2,6 +2,7 @@ import { GiAstronautHelmet } from "react-icons/gi";
 import styles from "./stylesLogin.module.css";
 import { PiPlanetDuotone } from "react-icons/pi";
 import imgLogin from "../../public/Free-Astronaut.jpg";
+import { BsRocketTakeoff } from "react-icons/bs";
 
 export default function Login() {
   return (
@@ -18,18 +19,32 @@ export default function Login() {
           <p>E site para encontrar novo mundo pela internet</p>
 
           <img src={imgLogin} alt="astronauta" />
+          <button>Cadastre AQUI!</button>
         </div>
         <div className={styles.caixaLogin}>
           <i>
             <GiAstronautHelmet />
           </i>
           <p>Login</p>
-          <div>
+          <div className={styles.contentLogin}>
             <p>E-mail</p>
 
-            <input type="text" />
+            <input type="email" />
             <p>Senha</p>
             <input type="password" />
+
+            <p>
+              <button>
+                Decolar{" "}
+                <i>
+                  <BsRocketTakeoff />
+                </i>
+              </button>
+            </p>
+
+            <p>
+              <input type="checkbox" /> Lembrar-me
+            </p>
           </div>
         </div>
       </div>
